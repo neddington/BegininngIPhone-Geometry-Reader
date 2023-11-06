a2c8502ca53c33931f0dc867354055d55a23a1b6
neddington <neddington@cascadia.edu>
2023-11-06 12:24:06 -0800

    Initial Commit

---
+++ BegininngIPhone Geometry Reader/BegininngIPhone_Geometry_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BegininngIPhone_Geometry_ReaderApp.swift
+//  BegininngIPhone Geometry Reader
+//
+//  Created by Eddington, Nick on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct BegininngIPhone_Geometry_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
